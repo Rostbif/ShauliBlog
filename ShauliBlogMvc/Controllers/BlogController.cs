f9075068346aec852cb7a7b65eec97f0b4700971
ofir adany <ofiradany@gmail.com>
2016-08-29 18:12:49 +0300

    some changes
    add some changes

--- ShauliBlogMvc/Controllers/BlogController.cs
+++ ShauliBlogMvc/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShauliBlogMvc.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,13 @@ namespace ShauliBlogMvc.Controllers
 {
     public class BlogController : Controller
     {
+        private BlogContext db = new BlogContext();
+
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
+
+            return View(db.Posts.ToList());
         }
     }
 }
