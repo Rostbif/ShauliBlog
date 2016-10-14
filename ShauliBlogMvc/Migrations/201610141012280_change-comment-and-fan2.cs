13ba611fe0843a32f959fe6ea2c013b58da74363
ofir adany <ofiradany@gmail.com>
2016-10-14 13:29:33 +0300

    add the changes after rebase

---
+++ ShauliBlogMvc/Migrations/201610141012280_change-comment-and-fan2.cs
@@ -0,0 +1,16 @@
+namespace ShauliBlogMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changecommentandfan2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- ShauliBlogMvc/Models/Comment.cs
+++ ShauliBlogMvc/Models/Comment.cs
@@ -10,8 +10,8 @@ namespace ShauliBlogMvc.Models
         public int ID { get; set; }
         public int PostID { get; set; }
         public string Title { get; set; }
-        public virtual Fan Author { get; set; }
         public string Content { get; set; }
         public virtual Post Post { get; set; }
+        public virtual Fan Author { get; set; }
     }
 }
