04d67c1e81a1a72b5a26c5635a98bd74e4ad1a20
ofir adany <ofiradany@gmail.com>
2016-10-29 19:11:36 +0300

    add the canvas

---
+++ ShauliBlogMvc/Scripts/Post/post_index.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var canvases = document.getElementsByClassName("canvas");
+    // run on each canvas and draw on it
+    for (var i = 1; i < canvases.length; i++){
+        var ctx = canvases[i].getContext("2d");
+        ctx.font = "18px Arial";
+        ctx.fillText("no image", 10, 50);
+    }
+}
+);
