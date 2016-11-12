5495e138f3584f8c033b9fcf7f2f11305644e4bc
Noam Roz <AvinoamRoz@gmail.com>
2016-11-12 22:55:32 +0200

    Adi first commit

--- ShauliBlogMvc/Models/Fan.cs
+++ ShauliBlogMvc/Models/Fan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,12 @@ namespace ShauliBlogMvc.Models
     public class Fan
     {
         public int ID { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Sex { get; set; }
+        [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
         public int Seniority { get; set; }
         public string Address { get; set; }
